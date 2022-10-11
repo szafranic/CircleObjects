660b363cd9eac99eb4e4b9c40d7eadfe7409cbdb
szafranic <ryan.szafranic@gmail.com>
2022-10-10 23:26:24 -0400

    initial

---
+++ Circle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleObjects
+{
+	internal class Circle
+	{
+		public Circle(double radius)
+		{
+
+		}
+		public double CalculateCircumference(double radius)
+		{
+			return radius * Math.PI * 2;
+		}
+		public string CalculateFormattedCircumference(double radius)
+		{
+			return "the circumference of your circle is: "+ FormatNumber(CalculateCircumference(radius));
+		}
+		public double CalculateArea(double radius)
+		{
+			return Math.PI * (radius * radius);
+		}
+		public string CalculateFormattedArea(double radius)
+		{
+			return "the area of your circle is: " + FormatNumber(CalculateArea(radius));
+		}
+		public string FormatNumber(double x)
+		{
+			return Math.Round(x, 2)+"";
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿namespace CircleObjects
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			int count = 0;
+			double radius;
+			
+			bool goOn = true;
+			while (goOn)
+			{
+				Console.Write("please enter a radius: ");
+				try
+				{
+					radius = double.Parse(Console.ReadLine());
+				}
+				catch (FormatException)
+				{
+					Console.WriteLine("your input was not a valid number");
+					continue;
+				}
+				count++;
+				Circle circle = new Circle(radius);
+				Console.WriteLine(circle.CalculateFormattedCircumference(radius) +" and "+ circle.CalculateFormattedArea(radius));
+				goOn = AskToContinue();
+			}
+			Console.WriteLine("later. you made "+count+" circles");
+		}
+		public static bool AskToContinue()
+		{
+			Console.WriteLine("would you like to do another? (y/n)");
+			string input = Console.ReadLine().ToLower();
+			if (input == "y" || input == "yes")
+			{
+				return true;
+
+			}
+			else if (input == "n" || input == "no")
+			{
+				return false;
+			}
+			else
+			{
+				Console.WriteLine("try again (y/n)");
+				return AskToContinue();
+			}
+		}
+	}
+}
